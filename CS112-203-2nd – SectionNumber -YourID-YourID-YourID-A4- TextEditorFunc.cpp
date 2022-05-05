8b60c8ee37a97150080934344ab04736275b64f8
Hossam Ahmed Fouad <57450146+HossamAhmedFouad@users.noreply.github.com>
2022-05-05 14:23:08 +0200

    Create CS112-203-2nd – SectionNumber -YourID-YourID-YourID-A4- TextEditorFunc.cpp
    Created Text Editor Functions Files

---
+++ CS112-203-2nd – SectionNumber -YourID-YourID-YourID-A4- TextEditorFunc.cpp
@@ -0,0 +1,8 @@
+// FCAI – Programming 1 – 2022 - Assignment 4
+// Program Name: CS112-203-2nd – SectionNumber -YourID-YourID-YourID-A4- TextEditorFunc.cpp
+// Last Modification Date: 5/5/2022
+// Author1 and ID and Group: Hossam Ahmed Fouad 20210124
+// Author2 and ID and Group: Abdelrahman Atef   20210227
+// Author3 and ID and Group: Omar Mohamed 	    20210272
+// Teaching Assistant: xxxxx xxxxx
+// Purpose:Perform Text Edit On txt files
